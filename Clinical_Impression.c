7b2335bc9bf2cfbe06125fdb44ddc9d3cb6e221f
Kovie <40118781+thekovie@users.noreply.github.com>
2022-05-14 00:04:30 +0800

    Update Certification Comment

--- Clinical_Impression.c
+++ Clinical_Impression.c
@@ -100,3 +100,14 @@ int main() {
 
     return 0;
 }
+
+/*************************************************************************************************
+    This is to certify that this project is our own work, based on our personal efforts in studying and
+    applying the concepts learned. We have constructed the functions and their respective algorithms
+    and corresponding code by ourselves. The program was run, tested, and debugged by our own efforts. We
+    further certify that we have not copied in part or whole or otherwise plagiarized the work of other
+    students and/or persons.
+    
+    John Kovie L. Niño, DLSU ID# 12109975
+    Reign Elaiza D. Larraquel, DLSU ID# <number>
+*************************************************************************************************/
